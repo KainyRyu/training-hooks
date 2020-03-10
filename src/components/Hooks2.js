63b9ffca0e129e2245440f1f76c195507bc54f93
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-10 14:30:34 +0000

    another way of couter

--- src/components/Hooks2.js
+++ src/components/Hooks2.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect }from 'react'
 
 export default function Hooks2() {
+    useEffect(() => {
+
+    })
+
     return (
         <div>
-            
+            <h1>{couter}</h1>
+            <button onClick={increase}></button>
         </div>
     )
 }
