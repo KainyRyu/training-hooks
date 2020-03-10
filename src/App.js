22391b432ef2158324b0892acdbd7893fd4a681b
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-10 14:28:21 +0000

    counter update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Hooks1 from './components/Hooks1'
+// import Hooks2 from './components/Hooks2'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Hooks1 />
     </div>
   );
 }
--- src/components/Hooks1.js
+++ src/components/Hooks1.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Hooks1() {
+    const [counter, setCounter] = useState(0);
+
+    function increase() {
+        setCounter(counter + 1);
+    }
+    
+    function decrease() {
+        setCounter(counter - 1);
+    }
+
     return (
         <div>
-            
+            <h1>{counter}</h1>
+            <button onClick={increase}> + </button>
+            <button onClick={decrease}> - </button>
         </div>
     )
 }
