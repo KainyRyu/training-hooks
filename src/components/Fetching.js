c235db8c72dcbe2cf5d67c19c25fc6fdf18b4e95
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-11 15:24:58 +0000

    randomising the meme

--- src/components/Fetching.js
+++ src/components/Fetching.js
@@ -2,40 +2,43 @@ import React, { useState, useEffect } from 'react';
 
 export default function Fetching() {
     const [meme, setMeme] = useState('');
+    const [name, setName] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        getMeme()
+        isLoading && getMeme();
         async function getMeme() {
             const response = await fetch('https://api.imgflip.com/get_memes');
             // const data = await response.json();
             // setMeme(data.url)      -> destructure ->
             const { data } = await response.json();
-            // setMeme(data);
+            const generated = data.memes[randomNum(0, 99)]
             setIsLoading(false);
-            setMeme(data.memes[oneToHundred(0, 99)].url)
-            console.log(data.memes[oneToHundred(0, 99)].url);
+            setMeme(generated.url)
+            setName(generated.name)
+            console.log(name, meme)
         }
     },[isLoading]);
-
-    function oneToHundred(min, max) {
+    
+    function randomNum(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-
+    
     function handleLoading() {
         setIsLoading('Loading...');
     }
-
+    
     return (
         <div>
             <h1>Random Meme</h1>
             <button onClick={handleLoading}>Next</button>
             {
                 isLoading ? 
-                    <div>{isLoading}</div> : 
-                    meme ?
-                        <div>
-                            <img src={meme} alt='meme' />
+                <div>{isLoading}</div> : 
+                meme ?
+                <div>
+                            <h2>{name}</h2>
+                            <img src={meme} alt='meme' style={{ width: '300px', margin: '5vh auto'}} />
                         </div> :
                         null
             }
