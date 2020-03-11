db43c6ea8d07d5150e6ee8914f9d8e29ccd3a3d5
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-11 08:11:17 +0000

    generating random meme!!

--- src/components/Fetching.js
+++ src/components/Fetching.js
@@ -5,14 +5,40 @@ export default function Fetching() {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+        getMeme()
         async function getMeme() {
             const response = await fetch('https://api.imgflip.com/get_memes');
-
+            // const data = await response.json();
+            // setMeme(data.url)      -> destructure ->
+            const { data } = await response.json();
+            // setMeme(data);
+            setIsLoading(false);
+            setMeme(data.memes[oneToHundred(0, 99)].url)
+            console.log(data.memes[oneToHundred(0, 99)].url);
         }
-    })
+    },[isLoading]);
+
+    function oneToHundred(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function handleLoading() {
+        setIsLoading('Loading...');
+    }
+
     return (
         <div>
-            <p>{isLoading}</p>
+            <h1>Random Meme</h1>
+            <button onClick={handleLoading}>Next</button>
+            {
+                isLoading ? 
+                    <div>{isLoading}</div> : 
+                    meme ?
+                        <div>
+                            <img src={meme} alt='meme' />
+                        </div> :
+                        null
+            }
         </div>
     )
 }
